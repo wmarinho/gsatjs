9a9a4215cf4e6334b59fc91c3da1b08334707a09
EC2 Default User <ec2-user@ip-172-31-31-109.sa-east-1.compute.internal>
2014-05-10 06:01:28 +0000

    .

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,26 @@ angular.module('gsatjsApp')
       'AngularJS',
       'Karma'
     ];
-  });
+  }).controller('NavController', function ($scope, $location, $modal) {
+    $scope.isCollapsed = true;
+	$scope.dashboards = ['Dashboard1', 'Dashboard2', 'Dashboard3'];
+	$scope.user = "Wellington Marinho";
+	$scope.company = "Globosat";
+	 
+    $scope.$on('$routeChangeSuccess', function () {
+        $scope.isCollapsed = true;
+    });
+		
+	
+	$scope.open = function () {
+		var modalInstance = $modal.open({
+		  templateUrl: 'perfilUsuario.html',
+		  controller: ModalInstanceCtrl		  
+    });   
+  };
+  var ModalInstanceCtrl = function ($scope, $modalInstance) {  
+	  $scope.close = function () {
+		$modalInstance.close();
+	  };
+  };
+});
